3d55e47301bde01a7b16bfa5c2395a88fc5bee7b
Yuan <heyuan@unionpaysmart.com>
2013-09-03 16:11:28 +0800

    [UPDATE]upload class and source

---
+++ HYSegmentedControl.h
@@ -0,0 +1,22 @@
+//
+//  ICPSegmentedControl.h
+//
+//  Created by Yuan on 13-9-3.
+//  http://www.heyuan110.com
+//
+
+#import <UIKit/UIKit.h>
+
+typedef void(^selectedBlcok) (NSInteger  segmentIndex);
+
+@interface HYSegmentedControl : UIView
+@property (nonatomic) CGFloat cornerRadius;
+@property (nonatomic) CGFloat borderWidth;
+@property (nonatomic,strong) UIColor *bgColor;
+@property (nonatomic,strong) UIColor *itemBackGroundColor;
+@property (nonatomic,strong) UIColor *textColor;
+@property (nonatomic,strong) UIColor *textSelectColor;
+
+- (id)initWithFrame:(CGRect)frame items:(NSArray*)items andSelectionBlock:(selectedBlcok)block;
+
+@end
--- README.md
+++ README.md
@@ -2,3 +2,6 @@ HYSegmentedControl
 ==================
 
 custom ios7 style segmentedcontrol
+
+截图
+<img src="jt.png">
